52c5d2929a493a871b03edcc7106bb9321f06824
Hélio S P Júnior <heliospjunior@gmail.com>
2026-01-08 22:51:26 -0300

    add two staging models, refactor dim_costumers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,22 +1,13 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+    SELECT * FROM {{ref('stg_jaffle_shop__customers')}}
 
-    from {{ source('jaffle_shop', 'raw_customers') }}
+   
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from rawdb.jaffle_shop.orders
+    SELECT * FROM {{ref('stg_jaffle_shop__orders')}}
 
 ),
 
